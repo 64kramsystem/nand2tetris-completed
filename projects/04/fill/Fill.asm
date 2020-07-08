4e66092cffd4e7e267b33703c7e843a3c55b3166
Saverio Miroddi <saverio.pub2@gmail.com>
2020-07-08 22:37:39 +0200

    Project 04: Completed

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,45 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(MAIN_LOOP)
+@SCREEN            // store top of screen into @screen_location
+D=A
+@screen_location
+M=D
+
+@KBD               // read key into D
+D=M
+
+// If no key is pressed, the value read is the color
+(DETERMINE_COLOR)
+@STORE_COLOR
+D; JEQ
+
+// Otherwise, convert the value to -1
+D=-1
+
+(STORE_COLOR)
+@color             // 9.
+M=D
+
+(FILL_SCREEN)
+@screen_location   // 11. if i == KBD; goto MAIN_LOOP
+D=M
+@KBD               // 13.
+D=D-A
+@MAIN_LOOP
+D; JEQ
+
+@color             // 17. load the color
+D=M
+@screen_location   // 19. load the screen pointer
+A=M
+M=D                // store the color
+
+D=A+1              // 22. increase the screen pointer
+@screen_location
+M=D
+
+@FILL_SCREEN // restart the loop
+0; JMP
